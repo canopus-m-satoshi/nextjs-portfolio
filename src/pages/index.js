374c8e5d34618eb407216d21c0afd14495296204
Satoshi Matsushima <>
2022-07-30 16:32:31 +0900

    created blog page and dummy blogs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,14 @@
+import Link from 'next/link'
+import * as style from '../styles/index.module.css'
+
 const Index = () => {
-  return <div>Index</div>
+  return (
+    <div>
+      <h1 className={style.h1Text}>Index</h1>
+      <Link href="/contact">
+        <a>To Contact</a>
+      </Link>
+    </div>
+  )
 }
 export default Index
